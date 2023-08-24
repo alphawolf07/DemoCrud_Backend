66a377046f3407cd4c66d0425c9903939c6870b9
Rohit Kumar Sadhukhan <rohit.sadhukhan1234@gmail.com>
2023-08-24 18:17:18 +0530

    backend done

--- Demo/Controllers/CrudController.cs
+++ Demo/Controllers/CrudController.cs
@@ -1,5 +1,6 @@
 ﻿using Demo.Entites;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace Demo.Controllers
@@ -19,7 +20,7 @@ namespace Demo.Controllers
             if(_context.CrudEntity==null)
             {
                 //Checking if database exists or not
-                return BadRequest("Database not found!");
+                return NotFound("Database not found!");
             }
             try
             {               
@@ -32,5 +33,113 @@ namespace Demo.Controllers
                 return StatusCode(500,ex.Message);
             }
         }
+
+
+        [HttpGet, Route("/GetAllStudent")]
+        public IActionResult GetAll()
+        {
+            if (_context.CrudEntity == null)
+            {
+                //Checking if database exists or not
+                return NotFound("Database not found!");
+            }
+            try
+            {
+               // _context.CrudEntity.Add(student);
+                //_context.SaveChanges();
+                return Ok(_context.CrudEntity.ToList());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+
+        [HttpGet, Route("/GetStudent/{id}")]
+        public IActionResult GetStudent(String id)
+        {
+            if (_context.CrudEntity == null)
+            {
+                //Checking if database exists or not
+                return NotFound("Database not found!");
+            }
+            try
+            {
+               var student=_context.CrudEntity.Find(id);
+                if(student == null)
+                {
+                    return NotFound();
+                }
+                return Ok(student);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+
+        [HttpDelete, Route("/DeleteStudent/{id}")]
+        public IActionResult DeleteStudent(String id)
+        {
+            if (_context.CrudEntity == null)
+            {
+                //Checking if database exists or not
+                return NotFound("Database not found!");
+            }
+            try
+            {
+               var student=_context.CrudEntity.Find(id);
+                if(student == null)
+                {
+                    return NotFound() ;
+                }
+                _context.CrudEntity.Remove(student);
+                _context.SaveChanges();
+                return StatusCode(200, new JsonResult("Student data deleted"));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+
+        [HttpPut, Route("/PutStudent/{id}")]
+        public IActionResult PutStudent(String id,CrudEntity student)
+        {
+            if (_context.CrudEntity == null)
+            {
+                //Checking if database exists or not
+                return NotFound("Database not found!");
+            }
+            try
+            {
+               if(id != student.Username)
+               {
+                    return BadRequest();
+               }
+               if(!crudExist(id))
+                {
+                    return NotFound();
+                }
+                _context.Entry(student).State = EntityState.Modified;
+                _context.SaveChanges();
+                return Ok("Successfully updated");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        private bool crudExist(string id)
+        {
+            return (_context.CrudEntity?.Any(e => e.Username == id)).GetValueOrDefault();
+        }
+
     }
 }
